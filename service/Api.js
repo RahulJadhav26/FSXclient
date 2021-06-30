3c2b3067b5c59d624f9848220f42631d0d2fa55f
RahulJadhav2611 <rrahuljadhav.rj@gmail.com>
2021-06-30 17:12:57 -0400

    v3 FINAL

--- service/Api.js
+++ service/Api.js
@@ -3,9 +3,9 @@ import axios from 'axios'
 export default () => {
   return axios.create({
   // Rahul
-    // baseURL: 'http://44.192.63.76:5005/'
+    baseURL: 'http://44.192.63.76:5005/'
   //  Sid
-    baseURL: 'http://54.225.25.229:5005/'
+    // baseURL: 'http://54.225.25.229:5005/'
     // baseURL: 'http://localhost:5005/'
   })
 }
